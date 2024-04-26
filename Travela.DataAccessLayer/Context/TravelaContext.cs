19f1f445b9a4a8e64282563e7348633878077e9d
Türkay Sağır <ates_turkay@hotmail.com>
2024-04-26 20:42:58 +0300

    Add project files.

---
+++ Travela.DataAccessLayer/Context/TravelaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Travela.EntityLayer.Concrete;
+
+namespace Travela.DataAccessLayer.Context
+{
+    public class TravelaContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-ER46C3V;initial catalog=TravelaDb;integrated security=true");
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
